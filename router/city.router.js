6bbd8b7a0e55b48889e127654410d67476973364
Ferny1011 <fer-ney1011@outlook.com>
2023-09-21 22:47:33 -0300

    fixes

--- router/city.router.js
+++ router/city.router.js
@@ -7,7 +7,7 @@ const router = express.Router();
 const { getCities, createCity, getCityById, updateCity, deleteCity } = cityController;
 
 router.get('/', getCities);
-router.post('/', passport.authenticate('jwt', { session: false }), isAdmin, createCity);
+router.post('/', passport.authenticate('jwt', { session: false }), createCity);
 router.get('/:id', getCityById);
 router.put('/:id', passport.authenticate('jwt', { session: false }), isAdmin, updateCity);
 router.delete('/:id', passport.authenticate('jwt', { session: false }), isAdmin, deleteCity);
